ac98dcf3c9f8f26658ea866d9f8da042296b171a
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-22 00:01:12 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Николай Никитин on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
